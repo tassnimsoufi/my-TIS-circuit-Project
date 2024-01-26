5855892e545b25bb230810653f9dcfd3c157767e
tassnim soufi <tassnimsoufi@gmail.com>
2024-01-26 21:51:32 +0100

    THIS IS MY FIRST PUSH

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.style === "navb") {
+      x.style.display = " none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
